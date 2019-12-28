566a4fc61b134bc25957c38ebb4fdaaeaeed2b42
vinod <vinod@DESKTOP-B570H7G>
2019-12-28 09:49:51 -0800

    Made changes to accept cross origin requests.

--- DSR/src/main/java/com/vinod/app/controller/EmployeController.java
+++ DSR/src/main/java/com/vinod/app/controller/EmployeController.java
@@ -3,6 +3,7 @@ package com.vinod.app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.vinod.app.model.Employee;
 import com.vinod.app.service.EmployeService;
 
 @RestController
+@CrossOrigin
 public class EmployeController {
 
 	@Autowired
